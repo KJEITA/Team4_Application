7fb6929f3b0c99975b057c28c404148434ec08b2
Sergey <serega.putkin@yandex.ru>
2019-11-28 19:07:11 +0300

    Импортировал LoginActivity

---
+++ app/src/main/java/bonch/dev/team4_application/ui/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package bonch.dev.team4_application.ui.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
