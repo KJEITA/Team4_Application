6b46ec478c2c903e00549c7be0aa4e5f8e8b0ce8
Valeria <muldtlera0@gmail.com>
2019-12-07 18:53:54 +0300

    Try to do Statistics Window

--- app/src/main/java/bonch/dev/team4_application/ui/home/StaticFragment.kt
+++ app/src/main/java/bonch/dev/team4_application/ui/home/StaticFragment.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import bonch.dev.team4_application.R
+import bonch.dev.team4_application.ui.home.StatisticsAdapter
+import kotlinx.android.synthetic.main.fragment_my_statistics.*
 
 class StaticFragment : DialogFragment() {
+
+    private lateinit var staticsRecyclerView: RecyclerView
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -21,6 +28,11 @@ class StaticFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_my_statistics, container, false)
+
+//        staticsRecyclerView = view.findViewById(R.id.article_recycler_view)
+//        staticsRecyclerView.layoutManager = LinearLayoutManager(container!!.context)
+//        staticsRecyclerView.adapter = StatisticsAdapter()
+
         return view
     }
 }
